9e2fb525e4047883b01a45dfa12d2404a9b32248
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-01-22 18:27:12 -0500

    start screen, game over screen

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ class Player {
         this.y += 15;
         break;
       case "ArrowLeft":
-        this.y -= 15;
+        this.x -= 15;
         break;
       case "ArrowRight":
-        this.y += 15;
+        this.x += 15;
         break;
     }
   }
@@ -37,8 +37,8 @@ let player = new Player();
 
 class Obstacle {
   constructor() {
-    this.x = 0;
-    this.y = 0;
+    this.x = Math.round(Math.random() * canvas.width - this.w);
+    this.y = Math.round(Math.random() * canvas.height - this.w);
     this.w = 25;
     this.h = 25;
     this.color = "red";
@@ -46,6 +46,11 @@ class Obstacle {
 }
 
 let object = new Obstacle();
+
+function spawnObstacle() {
+  ctx.fillStyle = object.color
+  ctx.fillRect = (object.x, object.y, object.w, object.h)
+}
 
 // Bringing over event listener
 
@@ -66,9 +71,26 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-// Add a timer to end the game
+// Add a timer to end the game -- NOT SURE WHERE TO PUT IT
+// myInterval = setInterval(function() {
+// timer--;
+// }, 1000);
+// clearInterval(myInterval);
+let timer = 30;
 
-let timer = 
+// Detect collision formula
+function detectCollision(player, obj) {
+  if (
+    player.x < obj.x + obj.w &&
+    player.x + player.w > obj.x &&
+    player.y < obj.y + obj.h &&
+    player.y + player.h > obj.y
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Make a starting screen
 
@@ -87,9 +109,35 @@ function startScreen() {
   document.addEventListener("click", startGame);
 }
 
-startScreen();
+// startScreen();
 
 function startGame() {
-
+  setInterval(function () {
+    timer--;
+  }, 1000);
   document.removeEventListener("click", startGame);
+}
+function gameOver() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "30px Arial";
+  ctx.textAlign = "center";
+  ctx.fillStyle = "black";
+  ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2);
+  ctx.fillText("Final score: ", canvas.width / 2, canvas.height / 2 + 50);
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.w, player.h);
+  spawnObstacle()
+  ctx.fillStyle = "black"
+  ctx.font = "20px Arial"
+  ctx.textAlign = "right"
+  ctx.fillText("text",x,y)
+  ctx.fillText("text",x,y)
+  
 }
