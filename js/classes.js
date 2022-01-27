31aae994133434de31a27deabac814f6514847c8
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-01-27 15:03:03 -0500

    v 1.0.7

--- js/classes.js
+++ js/classes.js
@@ -28,9 +28,8 @@ class Player {
   drawPlayer() {
     let playerImage = new Image();
     playerImage.src = "images/net.png";
-    playerImage.onload = () => {
-      ctx.drawImage(playerImage, this.x, this.y, this.w, this.h);
-    };
+    playerImage.onload = () => {};
+    ctx.drawImage(playerImage, this.x, this.y, this.w, this.h);
   }
 }
 // Target butterfly class
@@ -44,9 +43,8 @@ class Butterfly {
   drawButterfly() {
     let butterflyImage = new Image();
     butterflyImage.src = "images/butterfly.png";
-    butterflyImage.onload = () => {
-      ctx.drawImage(butterflyImage, this.x, this.y, this.w, this.h);
-    };
+    butterflyImage.onload = () => {};
+    ctx.drawImage(butterflyImage, this.x, this.y, this.w, this.h);
   }
   respawnButterfly() {
     target = new Butterfly();
@@ -63,9 +61,8 @@ class Bonus {
   drawGoldenButterfly() {
     let bonusImage = new Image();
     bonusImage.src = "images/gold.png";
-    bonusImage.onload = () => {
-      ctx.drawImage(bonusImage, this.x, this.y, this.w, this.h);
-    };
+    bonusImage.onload = () => {};
+    ctx.drawImage(bonusImage, this.x, this.y, this.w, this.h);
   }
   flyAcross() {
     this.y = this.y - 3;
